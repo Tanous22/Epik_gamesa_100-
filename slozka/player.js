f1f8c69cb62b58f1c492449a5d2c7fbda398ee14
Tanous22 <rulikda@eskola.eu>
2026-01-29 22:34:40 +0100

    Přidání space a shift (a snaha opravit SHIFT + W + SPACE + A)

--- slozka/player.js
+++ slozka/player.js
@@ -16,11 +16,17 @@ export class Player {
         this.moveBackward = false;
         this.moveLeft = false;
         this.moveRight = false;
+        this.isRunning = false;
 
         // 3. Event Listenery (Klávesnice)
         // Používáme .bind(this), aby funkce věděla, kdo je "já" (hráč)
         document.addEventListener('keydown', this.onKeyDown.bind(this));
         document.addEventListener('keyup', this.onKeyUp.bind(this));
+
+        this.velocityY = 0;
+        this.gravity = 0.005;
+        this.jumpStrengh = 0.25;
+        this.canJump = false;
 
     }
 
@@ -31,6 +37,13 @@ export class Player {
                 case "KeyA": this.moveLeft = true; break;
                 case "KeyS": this.moveBackward = true; break;
                 case "KeyD": this.moveRight = true; break;
+                case "ShiftLeft": this.isRunning = true; break;
+                case "Space":
+                    if (this.canJump === true){
+                        this.velocityY = this.jumpStrengh;
+                        this.canJump = false;
+                    }
+                    break;
             }
         }
 
@@ -41,17 +54,28 @@ export class Player {
                 case "KeyA": this.moveLeft = false; break;
                 case "KeyS": this.moveBackward = false; break;
                 case "KeyD": this.moveRight = false; break;
+                case "ShiftLeft": this.isRunning = false; break;
             }
         }
         
         update() {
             if (this.controls.isLocked){
-                const speed = 0.1;
+                const speed = this.isRunning ? 0.25 : 0.15;
 
                 if (this.moveForward) this.controls.moveForward(speed);
                 if (this.moveBackward) this.controls.moveForward(-speed);
                 if (this.moveRight) this.controls.moveRight(speed);
                 if (this.moveLeft) this.controls.moveRight(-speed);
+
+                //Gravitace
+                this.velocityY -= this.gravity;
+                this.camera.position.y += this.velocityY;
+                //Ošetření kolize s podlahou (potom kolize i s objektama)
+                if (this.camera.position.y < 1.6){
+                    this.camera.position.y = 1.6;
+                    this.velocityY = 0;
+                    this.canJump = true;
+                }
             }
         }
 }
